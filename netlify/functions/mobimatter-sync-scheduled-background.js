05463b3b955e7a651aa497408c2e4f44b359ec67
giannakkas <giannakkas@hotmail.com>
2025-05-11 12:21:13 +0300

    Update mobimatter-sync-scheduled-background.js

--- netlify/functions/mobimatter-sync-scheduled-background.js
+++ netlify/functions/mobimatter-sync-scheduled-background.js
@@ -47,7 +47,7 @@ const buildDescription = (details) => {
   }
 
   return (`${planDetailsHtml}${additionalHtml}`)
-    .replace(/(<(br|p|div)[^>]*>\s*<\/(br|p|div)>|\s|<br\s*\/?\>)+$/gi, '')
+    .replace(/(<(br|p|div)[^>]*>\s*<\/(br|p|div)>|\s|<br\s*\/?>)+$/gi, '')
     .replace(/\s+$/, '')
     .trim();
 };
@@ -78,7 +78,7 @@ exports.handler = async () => {
 
     if (!Array.isArray(products)) throw new Error("Invalid product array");
 
-    for (const product of products.slice(0, 2000)) {
+    for (const product of products.slice(0, 20)) {
       const handle = `mobimatter-${product.uniqueId}`.toLowerCase();
       const details = getProductDetails(product);
       const title = details.PLAN_TITLE || product.productFamilyName || "Unnamed eSIM";
